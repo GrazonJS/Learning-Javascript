27084bfef979abf001648c1bfe5490a21153ee94
Graceshon JS <grashon.2003@gmail.com>
2022-12-12 23:09:28 +0530

    higher order function

--- day4.js
+++ day4.js
@@ -22,4 +22,18 @@ console.log(result2)
 
      //.filter loops and returns an array based onthe given conditions
 
-     
+
+//REDUCE
+
+let result3 = [1,2,3,4].reduce((prev, curr) => prev+curr,0 ) 
+console.log(result3)
+    //Need to do sum then use reduce
+    //here it adds the previous and current values  causing an accumulation 
+    /* like 1+2 becomes 3 
+       3+3 becomes 6
+       6+4 becomes 10
+       i uused 0 at the end which denotes the initial values to start the operation */
+
+actors.reduce((prev,curr)=> prev + curr.networth,0)
+       
+      
