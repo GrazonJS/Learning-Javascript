d22148711c531b636fc8af1faf8dc04d6595c6e9
Graceshon JS <grashon.2003@gmail.com>
2022-12-16 07:59:53 +0530

    DOM manipulation

--- day5.js
+++ day5.js
@@ -11,3 +11,31 @@ title.innerText = result
 
 title.innerHTML = '<p>HELLO</p>' // this changes the h2 tag to p tag NOTE: '.innerHTML'
 
+//Adding (or) manippulating tags in HTML using JS
+ title.style.color='red'
+
+ //Creating rock/paper/scissors game(logical)
+
+ let redDiv = document.getElementById('red')
+ let yellowDiv = document.getElementById('yellow')
+ let greenDiv = document.getElementById('green')
+
+//.onclick will pass the function when you click on that tag (dom way)
+
+ redDiv.onclick = () => console.log('red')
+ yellowDiv.onclick = () => console.log('yellow')
+ greenDiv.onclick = () => console.log('green')
+
+
+ //Using Query selector 
+let square = document.querySelectorAll('.color-square')
+
+//Using forEach (looping function)
+square.forEach(square => console.log(square.value))
+
+//Doing that .onclick thing using forEach()
+
+square.forEach(square.onclick = () => {
+    console.log(square.value)``
+})
+
